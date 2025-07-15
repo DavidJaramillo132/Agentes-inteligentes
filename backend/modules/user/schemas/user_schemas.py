5453a4a62398d161ae9844dbc2eb50101dbaffd4
David Jaramillo <djdavidjaramillo@gmail.com>
2025-07-15 14:50:49 -0500

    Ultimos cambios

---
+++ backend/modules/user/schemas/user_schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from ..model import UserModel
+
+
+
+class UserBaseSchema(BaseModel):
+    email: str
+    
+    
+class UserCreateSchema(UserBaseSchema):
+    password: str
+    
+    
+class UserResponseSchema(UserModel):
+    pass
+    
+    
+class UsersResponseSchema(BaseModel):
+    users : list[UserResponseSchema]
+    
+    
+class LoginUserSchema(UserBaseSchema):
+    access_token: str
