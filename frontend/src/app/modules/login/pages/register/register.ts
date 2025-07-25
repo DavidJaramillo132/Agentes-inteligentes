e0909652725cf964111fcdefcde9fb998289350e
David Jaramillo <djdavidjaramillo@gmail.com>
2025-07-24 19:30:02 -0500

    register

--- frontend/src/app/modules/login/pages/register/register.ts
+++ frontend/src/app/modules/login/pages/register/register.ts
@@ -15,8 +15,16 @@ export class Register {
 
     constructor(private router: Router) {}
 
-    registrar(): boolean {
-        if(!this.username || !this.password) {
+    registrar(): void {
+        if(this.validaciones_register()) {
+            alert(`Intentando registrar:\nUsuario: ${this.username}\nEmail: ${this.email}\nContraseña: ${this.password}`);
+            this.router.navigate(['/agents']);
+        }
+
+    }
+
+    validaciones_register(): boolean {
+                if(!this.username || !this.password || !this.email || !this.confirmPassword) {
             alert('Por favor, complete todos los campos.');
             return false;
         }
@@ -28,8 +36,20 @@ export class Register {
             alert('La contraseña debe tener entre 6 y 20 caracteres.');
             return false;
         }
+        if(this.password !== this.confirmPassword){
+            alert('Las contraseñas no coinciden.');
+            return false;
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(this.email)){
+            alert('Por favor, ingrese un correo electrónico válido.');
+            return false;
+        }
+
         return true;
     }
+
+
     
 
 
